70bd136fe4066c2fe891cee1669efb8846e1b675
kdg1986 <user@email.mail>
2020-02-14 22:20:58 +0900

    라우트 실습

--- chap11/src/App.js
+++ chap11/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route,Link} from 'react-router-dom'
+import Home,{About} from './Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <div>
+            <ul>
+                <li>
+                    <Link to="">홈</Link>
+                </li>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+            </ul>
+            <hr></hr>
+            <Route path="/" component={Home} exact={true}></Route>
+            <Route path="/about" component={About}></Route>             
+        </div>        
+    )
 }
+
 
 export default App;
---
+++ chap11/src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>홈</h1>
+            <p>홈, 그 페이지는 가장 먼저 보여지는 페이지.</p>
+        </div>
+    )
+}
+
+export const About = () => {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트 입니다.</p>
+        </div>
+    )
+}
+
+export default Home;
