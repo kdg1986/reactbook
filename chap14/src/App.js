d66800a3a1ede770f814f31e68a07410e521477e
kdg1986 <user@email.mail>
2020-02-25 22:06:50 +0900

    14장

---
+++ chap14/src/App.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+  const [data ,setData] = useState(null);
+
+  const onClick = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(res => {      
+      setData( res.data )
+    })
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      { data && <textarea rows={7} value={ JSON.stringify( data,null,2 )} readOnly={true}></textarea> }
+    </div>
+  )
+
+}
+
+
+export default App;
